3b8dd803a54e542ba24aedef18bdeb73fdee1ac6
Muteeb Haider <muteebhdr@gmail.com>
2025-08-03 12:04:52 +0500

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,88 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
+
 const Profile = () => {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+  const handleFileUpload = async () => {
+    const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+    const uploadPreset = "avatar"; // You can use your preset name or create one in Cloudinary.
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+    formData.append("cloud_name", cloudName);
+
+    try {
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.secure_url) {
+        console.log(data);
+
+        console.log("Cloudinary Image URL:", data.secure_url);
+        setFormData((prev) => ({ ...prev, avatar: data.secure_url }));
+      } else {
+        console.error("Upload failed", data);
+      }
+    } catch (err) {
+      console.error("Error uploading file:", err);
+    }
+  };
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+
+    if (selectedFile) {
+      const allowedTypes = [
+        "image/jpeg",
+        "image/png",
+        "image/jpg",
+        "image/webp",
+      ];
+
+      if (!allowedTypes.includes(selectedFile.type)) {
+        setError("Please select a valid image file (JPEG, PNG, JPG, or WEBP).");
+        return;
+      }
+
+      setFile(selectedFile); // Only set file if it's valid
+      setError(null);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-bold text-center my-7">Profile</h1>
       <form className=" flex flex-col gap-4">
+        <input
+          onChange={handleFileChange}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
-          src={currentUser.avatar}
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
+        {error && <p className="text-red-500 text-sm font-semibold self-center ">{error}</p>}
         <input
           type="text"
           placeholder="Username"
