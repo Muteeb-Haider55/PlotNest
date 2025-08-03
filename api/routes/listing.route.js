ab64b898f435bf707a2df0b8f6d9c7e4d1c377c8
Muteeb Haider <muteebhdr@gmail.com>
2025-08-03 18:53:20 +0500

    add create listing api route

---
+++ api/routes/listing.route.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createListing } from "../controllers/listing.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
+const router = Router();
+
+router.post("/create", verifyToken, createListing);
+
+export default router;
