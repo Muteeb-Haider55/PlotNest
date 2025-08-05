6803cfe939a593a6074ad82a65fa3e115529b3db
Muteeb Haider <muteebhdr@gmail.com>
2025-08-05 22:11:26 +0500

    add contact landlord functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,70 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import SwiperCore from "swiper";
-// import { Navigation } from "swiper/modules";
-// import "swiper/css/bundle";
-
-// const Listing = () => {
-//   SwiperCore.use([Navigation]);
-//   const [listing, setListing] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(false);
-
-//   const params = useParams();
-//   useEffect(() => {
-//     const fetchListing = async () => {
-//       try {
-//         setLoading(true);
-//         const res = await fetch(`/api/listing/get/${params.listingId}`);
-//         const data = await res.json();
-
-//         if (data.success === false) {
-//           setError(true);
-//           setLoading(false);
-//           return;
-//         }
-//         setError(false);
-//         setListing(data);
-//         setLoading(false);
-//       } catch (error) {
-//         setLoading(false);
-//         setError(true);
-//         console.log(error);
-//       }
-//     };
-//     fetchListing();
-//   }, [params.listingId]);
-//   return (
-//     <main>
-//       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
-//       {error && (
-//         <p className="text-center text-red-700 my-7 text-2xl">
-//           Something went wrong!
-//         </p>
-//       )}
-//       {listing && !loading && !error && (
-//         <div>
-//           <Swiper navigation>
-//             {listing.imageUrls.map((url) => (
-//               <SwiperSlide key={url}>
-//                 <div
-//                   className="h-[00px]"
-//                   style={{
-//                     background: `url(${url}) center no-repeat `,
-//                     backgroundSize: "cover",
-//                   }}
-//                 ></div>
-//               </SwiperSlide>
-//             ))}
-//           </Swiper>
-//         </div>
-//       )}
-//     </main>
-//   );
-// };
-
-// export default Listing;
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -81,15 +14,18 @@ import {
   FaParking,
   FaShare,
 } from "react-icons/fa";
+import Contact from "../components/Contact";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
+
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
   const [contact, setContact] = useState(false);
   const params = useParams();
+
   const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
