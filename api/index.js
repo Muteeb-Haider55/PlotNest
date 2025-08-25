075deba65b7024b8b2c20ba2e727c80894b094f5
Muteeb Haider <muteebhdr@gmail.com>
2025-08-25 21:47:59 +0500

    pre installation for vercel deployment

--- api/index.js
+++ api/index.js
@@ -16,8 +16,6 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
-
 const app = express();
 
 //middlewares
@@ -32,16 +30,9 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
-
-//deployment settings
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
-
-
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
